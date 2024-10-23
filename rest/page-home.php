d224d487f8197ed3ab85877663d8797e31777390
Luiz Mateus Sanches de oliveira <lzu.luizmateus@gmail.com>
2024-10-23 11:11:59 -0300

    Teste para ver se funcionou

--- rest/page-home.php
+++ rest/page-home.php
@@ -2,6 +2,7 @@
 // Template Name: Menu da Semana
 ?>
 <?php get_header(); ?>
+<h1>DEU TUDO CERTO</h1>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<?php if(have_rows('category_menu')): ?>
 			<?php include(TEMPLATEPATH . "/inc/cardapio.php"); ?>
