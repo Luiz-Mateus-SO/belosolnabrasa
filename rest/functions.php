b653982ba07faa44111f1697fd3ac4d71a4f3c73
Luiz Mateus Sanches de oliveira <lzu.luizmateus@gmail.com>
2024-10-25 16:20:50 -0300

    js fix

--- rest/functions.php
+++ rest/functions.php
@@ -1,29 +1,28 @@
 <?php 
-  function my_scripts() {
-    wp_register_scripts('closed', get_template_directory_uri() . '/js/closed.js', [], false, true)
-  }
-  add_actions('wp_enqueue_scripts', 'my_scripts');
+function my_scripts() {
+    wp_register_script('closed', get_template_directory_uri() . '/js/closed.js', [], false, true);
+    wp_enqueue_script('closed'); // Enqueue the script
+}
+add_action('wp_enqueue_scripts', 'my_scripts');
 
-  function belosol_css() {
-    wp_register_style('belosol-style', get_template_directory_uri() . 'style.css', array(), false, false)
-  }
-  add_actions('wp_enqueue_scripts', 'belosol_css');
+function belosol_css() {
+    wp_register_style('belosol-style', get_template_directory_uri() . '/style.css', array(), false, false);
+    wp_enqueue_style('belosol-style'); // Enqueue the style
+}
+add_action('wp_enqueue_scripts', 'belosol_css');
 
+// Funções para Limpar o Header
+remove_action('wp_head', 'rsd_link');
+remove_action('wp_head', 'wlwmanifest_link');
+remove_action('wp_head', 'start_post_rel_link', 10, 0);
+remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
+remove_action('wp_head', 'feed_links_extra', 3);
+remove_action('wp_head', 'wp_generator');
+remove_action('wp_head', 'print_emoji_detection_script', 7);
+remove_action('admin_print_scripts', 'print_emoji_detection_script');
+remove_action('wp_print_styles', 'print_emoji_styles');
+remove_action('admin_print_styles', 'print_emoji_styles');
 
-
-  // Funções para Limpar o Header
-  remove_action('wp_head', 'rsd_link');
-  remove_action('wp_head', 'wlwmanifest_link');
-  remove_action('wp_head', 'start_post_rel_link', 10, 0 );
-  remove_action('wp_head', 'adjacent_posts_rel_link_wp_head', 10, 0);
-  remove_action('wp_head', 'feed_links_extra', 3);
-  remove_action('wp_head', 'wp_generator');
-  remove_action('wp_head', 'print_emoji_detection_script', 7);
-  remove_action('admin_print_scripts', 'print_emoji_detection_script');
-  remove_action('wp_print_styles', 'print_emoji_styles');
-  remove_action('admin_print_styles', 'print_emoji_styles');
-
-  // Habilitar Menus
-  add_theme_support('menus');
-
+// Habilitar Menus
+add_theme_support('menus');
 ?>
