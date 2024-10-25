11021f7770dc3a5e6fb3fa8b3c6f7a7fb0bdabcd
Luiz Mateus Sanches de oliveira <lzu.luizmateus@gmail.com>
2024-10-25 15:47:37 -0300

    closed hour function add

--- rest/functions.php
+++ rest/functions.php
@@ -13,4 +13,16 @@
 
   // Habilitar Menus
   add_theme_support('menus');
+
+  // Função para carregar o arquivo fechado.js
+  function carregar_fechado_js() {
+      wp_enqueue_script(
+          'fechado-script', // Nome do script
+          get_template_directory_uri() . '/rest/js/fechado.js', // Caminho para o arquivo JS
+          array(), // Dependências (se houver)
+          null, // Versão do script (null para usar a versão do tema)
+          true // Colocar o script no footer
+      );
+  }
+  add_action('wp_enqueue_scripts', 'carregar_fechado_js');
 ?>
---
+++ rest/js/fechado.js
@@ -0,0 +1,32 @@
+function verificarHora() {
+  const elementoDia = document.getElementById('dia-da-semana');
+  const agora = new Date();
+  const horaAtual = agora.getHours();
+
+  if (horaAtual === 14) {
+      elementoDia.innerText = "Fechado";
+      clearInterval(intervaloHora);
+      configurarVerificacaoAposFechado();
+  } else if (horaAtual === 6) {
+      location.reload();
+  }
+}
+
+function configurarVerificacaoAposFechado() {
+  const agora = new Date();
+  const proximaSeisHoras = new Date(
+      agora.getFullYear(),
+      agora.getMonth(),
+      agora.getDate() + 1,
+      6, 0, 0
+  );
+
+  const tempoParaSeisHoras = proximaSeisHoras - agora;
+
+  setTimeout(() => {
+      location.reload();
+  }, tempoParaSeisHoras);
+}
+
+let intervaloHora = setInterval(verificarHora, 3600000);
+verificarHora();
