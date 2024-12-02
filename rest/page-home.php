2b9cfe4122489ed46f75edda782b8a305a20f29d
Luiz Mateus Sanches de oliveira <lzu.luizmateus@gmail.com>
2024-12-02 09:17:11 -0300

    fix acompanhamentos

--- rest/page-home.php
+++ rest/page-home.php
@@ -2,9 +2,9 @@
 // Template Name: Menu da Semana
 ?>
 <?php get_header(); ?>
-<section class="acompanhamentos-container">
-    <h2 class="acompanhamentos-titulo">Acompanhamentos do dia: </h2>
-    <p class="acompanhamentos-do-dia"><?php the_field('acompanhamentos'); ?></p>
+<section class="acompanhamentos-container" style="display: flex;justify-content: center;flex-direction: column;">
+    <h2 class="acompanhamentos-titulo" style="color: hsla(39,100%,68%,1);text-align: center;font-size: 1.5em;">Acompanhamentos do dia:</h2>
+    <p class="acompanhamentos-do-dia" style="color: white;text-align: center;font-size: 1.1em;"><?php the_field('acompanhamentos'); ?></p>
 </section>
 <?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 	<?php if(have_rows('category_menu')): ?>
