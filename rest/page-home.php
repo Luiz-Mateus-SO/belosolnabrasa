4bd9d209f286317698bbe5350615e408b6e79ca4
Luiz Mateus Sanches de oliveira <lzu.luizmateus@gmail.com>
2024-12-02 09:18:33 -0300

    spacing adjust acompanhamentos section

--- rest/page-home.php
+++ rest/page-home.php
@@ -2,7 +2,7 @@
 // Template Name: Menu da Semana
 ?>
 <?php get_header(); ?>
-<section class="acompanhamentos-container" style="display: flex;justify-content: center;flex-direction: column;">
+<section class="acompanhamentos-container" style="display: flex;justify-content: center;flex-direction: column;padding-top: 40px;">
     <h2 class="acompanhamentos-titulo" style="color: hsla(39,100%,68%,1);text-align: center;font-size: 1.5em;">Acompanhamentos do dia:</h2>
     <p class="acompanhamentos-do-dia" style="color: white;text-align: center;font-size: 1.1em;"><?php the_field('acompanhamentos'); ?></p>
 </section>
